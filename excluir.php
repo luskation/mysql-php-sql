292f7e7ca4bff0eaee42baedb0ee65f8d87817bf
Lucas Rodrigues <lucasor2006@gmail.com>
2026-06-19 11:29:22 -0300

    fix: termino das correções dos codigos php

--- excluir.php
+++ excluir.php
@@ -1,7 +1,8 @@
 <?php
 include("./config.php");
 $con = mysqli_connect($host, $login, $senha, $bd);
-$sql = "DELETE FROM dados_pessoais WHERE codigo=".$_GET["codigo"];
+$sql = "DELETE FROM Pet WHERE idPet=".$_GET["idPet"];
 mysqli_query($con, $sql);
+mysqli_close($con);
 header("location: ./index.php");
 ?>
--- incluir.php
+++ incluir.php
@@ -1,15 +1,24 @@
 <?php
   include("./config.php");
   $con = mysqli_connect($host, $login, $senha, $bd);
-  if(isset($_POST["codigo"])){
-    $sql = "SELECT codigo FROM dados_pessoais WHERE codigo=".$_POST["codigo"];
+  
+  // Captura os dados enviados pelo formulário
+  $nome_pet = $_POST["nome_pet"];
+  $especie = $_POST["especie"];
+  $raca = $_POST["raca"];
+  $data_nasc = empty($_POST["data_Nasciment"]) ? "NULL" : "'".$POST["data_Nasciment"]."'";
+  $cpf = $_POST["cpf"];
+
+  if(isset($_POST["idPet"])){
+    $sql = "SELECT idPet FROM Pet WHERE idPet=".$_POST["idPet"];
     $result = mysqli_query($con, $sql);
-    if(mysqli_num_rows($result)!=0){
-      $sql = "UPDATE dados_pessoais SET nome='".$_POST["nome"]."',ddd=".$_POST["ddd"].",telefone=".$_POST["telefone"]." WHERE codigo=".$_POST["codigo"];
+    if(mysqli_num_rows($result) != 0){
+      $sql = "UPDATE Pet SET nome_pet='$nome_pet', especie='$especie', raca='$raca', data_Nasciment=$data_nasc, cpf='$cpf' WHERE idPet=".$_POST["idPet"];
     }
   }else{
-    $sql = "INSERT INTO dados_pessoais VALUES (null,'".$_POST["nome"]."',".$_POST["ddd"].",".$_POST["telefone"].")";
+    $sql = "INSERT INTO Pet (idPet, nome_pet, especie, raca, data_Nasciment, cpf) VALUES (null, '$nome_pet', '$especie', '$raca', $data_nasc, '$cpf')";
   }
+  
   mysqli_query($con, $sql);
   mysqli_close($con);
   header("location: ./index.php");
