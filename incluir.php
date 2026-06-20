b4399a3087c38b4ac0b8335cfa13e7116a95b296
Thawan Santos <thawan.santos@youxgroup.com.br>
2026-06-20 16:18:55 -0300

    :bug: fix: Salvamento de Pets enviando o atributo "data_nascimento" de forma incorreta para o banco

--- incluir.php
+++ incluir.php
@@ -1,24 +1,24 @@
 <?php
   include("./config.php");
   $con = mysqli_connect($host, $login, $senha, $bd);
-  
+
   // Captura os dados enviados pelo formulário
   $nome_pet = $_POST["nome_pet"];
   $especie = $_POST["especie"];
   $raca = $_POST["raca"];
-  $data_nasc = empty($_POST["data_Nasciment"]) ? "NULL" : "'".$POST["data_Nasciment"]."'";
+  $data_nasc = empty($_POST["data_nascimento"]) ? "NULL" : "'".$POST["data_nascimento"]."'";
   $cpf = $_POST["cpf"];
 
   if(isset($_POST["idPet"])){
     $sql = "SELECT idPet FROM Pet WHERE idPet=".$_POST["idPet"];
     $result = mysqli_query($con, $sql);
     if(mysqli_num_rows($result) != 0){
-      $sql = "UPDATE Pet SET nome_pet='$nome_pet', especie='$especie', raca='$raca', data_Nasciment=$data_nasc, cpf='$cpf' WHERE idPet=".$_POST["idPet"];
+      $sql = "UPDATE Pet SET nome_pet='$nome_pet', especie='$especie', raca='$raca', data_nascimento=$data_nasc, cpf='$cpf' WHERE idPet=".$_POST["idPet"];
     }
-  }else{
-    $sql = "INSERT INTO Pet (idPet, nome_pet, especie, raca, data_Nasciment, cpf) VALUES (null, '$nome_pet', '$especie', '$raca', $data_nasc, '$cpf')";
+  } else{
+    $sql = "INSERT INTO Pet (idPet, nome_pet, especie, raca, data_nascimento, cpf) VALUES (null, '$nome_pet', '$especie', '$raca', $data_nasc, '$cpf')";
   }
-  
+
   mysqli_query($con, $sql);
   mysqli_close($con);
   header("location: ./index.php");
